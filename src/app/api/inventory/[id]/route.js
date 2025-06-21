00ccea86a7195c1270e69ad797a97baf869b711c
Siddhesh Shinde <shindesiddhesh.co.in@gmail.com>
2025-06-21 11:24:34 +0530

    fix: fixes

--- src/app/api/inventory/[id]/route.js
+++ src/app/api/inventory/[id]/route.js
@@ -1,35 +1,35 @@
-import { getInventory, setInventory } from '../route';
+// import { getInventory, setInventory } from '../route';
 
-export async function GET(request, { params }) {
-  const items = getInventory();
-  const item = items.find(item => item.id === Number(params.id));
-  if (!item) {
-    return Response.json({ error: 'Not found' }, { status: 404 });
-  }
-  return Response.json(item);
-}
+// export async function GET(request, { params }) {
+//   const items = getInventory();
+//   const item = items.find(item => item.id === Number(params.id));
+//   if (!item) {
+//     return Response.json({ error: 'Not found' }, { status: 404 });
+//   }
+//   return Response.json(item);
+// }
 
-export async function PUT(request, { params }) {
-  const items = getInventory();
-  const index = items.findIndex(item => item.id === Number(params.id));
-  if (index === -1) {
-    return Response.json({ error: 'Not found' }, { status: 404 });
-  }
+// export async function PUT(request, { params }) {
+//   const items = getInventory();
+//   const index = items.findIndex(item => item.id === Number(params.id));
+//   if (index === -1) {
+//     return Response.json({ error: 'Not found' }, { status: 404 });
+//   }
   
-  const updatedItem = await request.json();
-  items[index] = { ...items[index], ...updatedItem };
-  setInventory(items);
-  return Response.json(items[index]);
-}
+//   const updatedItem = await request.json();
+//   items[index] = { ...items[index], ...updatedItem };
+//   setInventory(items);
+//   return Response.json(items[index]);
+// }
 
-export async function DELETE(request, { params }) {
-  const items = getInventory();
-  const index = items.findIndex(item => item.id === Number(params.id));
-  if (index === -1) {
-    return Response.json({ error: 'Not found' }, { status: 404 });
-  }
+// export async function DELETE(request, { params }) {
+//   const items = getInventory();
+//   const index = items.findIndex(item => item.id === Number(params.id));
+//   if (index === -1) {
+//     return Response.json({ error: 'Not found' }, { status: 404 });
+//   }
   
-  items.splice(index, 1);
-  setInventory(items);
-  return Response.json({ message: 'Item deleted' });
-}
+//   items.splice(index, 1);
+//   setInventory(items);
+//   return Response.json({ message: 'Item deleted' });
+// }
